8f7630eb0666646d4af4cb115644c294df015ff3
Nagaba Shallot <nagabashalloti@gmail.com>
2026-05-06 20:18:01 +0300

    added solutions to the new questions

---
+++ 11.js
@@ -0,0 +1,19 @@
+// FizzBuzz
+function fizzBuzz(){
+    for(i=1; i <= 30; i++ ){
+        if (i%3==0 && i%5==0){
+            console.log('FizzBuzz')
+        }
+        else if (i%5==0){
+            console.log('Buzz')
+        }
+        else if(i%3==0 ){
+            console.log('Fizz')
+        }
+        else{
+            console.log(i);
+        }
+    }
+
+}
+fizzBuzz();
