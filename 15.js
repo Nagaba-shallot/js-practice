094c0b971c584fc64dcaaf3a47d361bcbbce3767
Nagaba Shallot <nagabashalloti@gmail.com>
2026-05-13 18:17:33 +0300

    formatted

--- 15.js
+++ 15.js
@@ -1,7 +1,7 @@
 //Multiplication table
-function timesTable(n){
-    for(i = 1; i <= 10; i ++){
-        console.log(`${n} * ${i} = ${n * i}`);
-    }
+function timesTable(n) {
+  for (i = 1; i <= 10; i++) {
+    console.log(`${n} * ${i} = ${n * i}`);
+  }
 }
-timesTable (7);
+timesTable(7);
